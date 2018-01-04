24d1630341310025e32507bb8acc1e6ca7a9b986
Jedai Saboteur <dev@paradoxinversion.com>
2018-01-04 11:24:46 -0800

    Added password hashing via bcrypt

--- src/actions/signUp.js
+++ src/actions/signUp.js
@@ -1,8 +1,10 @@
+import bcrypt from 'bcrypt'
 import db from '../db'
 
-export default function signUp(user) {
+export default async function signUp(user) {
+  const hashedPass = await bcrypt.hash(user.password, 10)
   return db.one(`INSERT INTO
                   users (name, email, password)
                  VALUES ($1, $2, $3)
-                 RETURNING * `, [user.name, user.email, user.password])
+                 RETURNING * `, [user.name, user.email, hashedPass])
 }
